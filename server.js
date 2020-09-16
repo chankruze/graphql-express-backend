9d42c052877b88940af994cdf5e392d457ed1d74
chankruze <chankruze@gmail.com>
2020-09-16 23:37:54 +0530

    Prepare Express Server
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ server.js
@@ -0,0 +1,26 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Wed Sep 16 2020 11:32:43 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema')
+const os = require('os')
+
+const app = express()
+
+app.use('/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+)
+
+const PORT = process.env.PORT || 5005
+const networkInterfaces = os.networkInterfaces()
+let SERV_URL = networkInterfaces.eth0[0].address
+
+app.listen(PORT, () => console.log(`Server on network: http://${SERV_URL}:${PORT}/`))
