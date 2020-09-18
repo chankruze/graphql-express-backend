f0ad045ef6e73b5bdce2840f311f82fc47346895
chankruze <chankruze@gmail.com>
2020-09-18 22:06:26 +0530

    Adds Frontend for Deployment
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ public/precache-manifest.2d463dbc751f2cba484980d58903f122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add2ba61b85c1b876fb97d8910dd283d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c27e341c1b3b4bbd2f7",
+    "url": "/static/css/main.bdbf1634.chunk.css"
+  },
+  {
+    "revision": "150e185c8457ec2864ef",
+    "url": "/static/js/2.57c350c3.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.57c350c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c27e341c1b3b4bbd2f7",
+    "url": "/static/js/main.5ead9a44.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "950ba66541181838b6c07f14d62e331a",
+    "url": "/static/media/logo.950ba665.png"
+  }
+]);
